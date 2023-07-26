19710b229ac09d403246312cb514820d40cfbdec
Cuong Le <77721179+tcuong1204@users.noreply.github.com>
2023-07-26 14:09:01 +0700

    bugfix/CuongLe_CucumberTag: Add tag and more option to run test cases for CucumberOptions (#5)

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -9,7 +9,11 @@ import org.junit.runner.RunWith;
                 "pretty", // make report readable format
                 "html:target/login.html", // html report will be generated after running in target/ directory with name login.html
                 "json:target/login.json"
-        }
+        },
+        tags = "", // Run with specific tag
+        publish = true, // Public report
+        name = {".with\\svalid."} // Run with scenario that match with regex
+
 )
 public class TestRunner {
 

1be57bff7e85aeb53c6e92f893f0ab27f18b95b3
Cuong Le <77721179+tcuong1204@users.noreply.github.com>
2023-07-26 10:20:23 +0700

    feature/CuongLe_Report: Add Cucumber reporting feature (#2)

---
+++ src/framework/java/tma/sharing/Utils/ReportHandler.java
@@ -0,0 +1,60 @@
+package tma.sharing.Utils;
+
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import net.masterthought.cucumber.Reportable;
+import net.masterthought.cucumber.json.support.Status;
+import net.masterthought.cucumber.presentation.PresentationMode;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ReportHandler {
+
+    public static void customReport() {
+
+        File reportOutputDirectory = new File("target");
+        List<String> jsonFiles = new ArrayList<>();
+        jsonFiles.add("target/login.json");
+
+        String buildNumber = "1";
+        String projectName = "Cucumber Project";
+
+        Configuration configuration = new Configuration(reportOutputDirectory, projectName);
+
+        // Optional configuration - check javadoc for details
+        configuration.addPresentationModes(PresentationMode.RUN_WITH_JENKINS);
+        configuration.addPresentationModes(PresentationMode.EXPAND_ALL_STEPS);
+
+        // Do not make scenario failed when step has status SKIPPED
+        configuration.setNotFailingStatuses(Collections.singleton(Status.SKIPPED));
+        configuration.setBuildNumber(buildNumber);
+
+        // Additional metadata presented on main page
+        configuration.addClassifications("Platform", "Windows");
+        configuration.addClassifications("Browser", "Chrome");
+        configuration.addClassifications("Branch", "release/1.0");
+
+        // optionally add metadata presented on main page via properties file
+//        List<String> classificationFiles = new ArrayList<>();
+//        classificationFiles.add("properties-1.properties");
+//        classificationFiles.add("properties-2.properties");
+//        configuration.addClassificationFiles(classificationFiles);
+
+        // optionally specify qualifiers for each of the report json files
+//        configuration.addPresentationModes(PresentationMode.PARALLEL_TESTING);
+//        configuration.setQualifier("cucumber-report-1","First report");
+//        configuration.setQualifier("cucumber-report-2","Second report");
+
+        ReportBuilder reportBuilder= new ReportBuilder(jsonFiles,configuration);
+        Reportable result= reportBuilder.generateReports();
+
+        // and here validate 'result' to decide what to do if report has failed
+        System.out.println("Passed scenarios: " + result.getPassedScenarios());
+        System.out.println("Report generated");
+
+    }
+
+}
